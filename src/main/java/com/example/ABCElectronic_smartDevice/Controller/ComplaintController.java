c4c52a82f6e387fa06f82919d5960446d798586c
purna <p.purnababu@gmail.com>
2023-11-06 23:03:18 +0530

    done

---
+++ src/main/java/com/example/ABCElectronic_smartDevice/Controller/ComplaintController.java
@@ -0,0 +1,49 @@
+package com.example.ABCElectronic_smartDevice.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ABCElectronic_smartDevice.entity.Client;
+import com.example.ABCElectronic_smartDevice.entity.Complaint;
+import com.example.ABCElectronic_smartDevice.entity.Engineer;
+import com.example.ABCElectronic_smartDevice.entity.Product;
+import com.example.ABCElectronic_smartDevice.exceptions.ResourceNotFoundException;
+import com.example.ABCElectronic_smartDevice.services.IComplaintService;
+
+@RestController
+@RequestMapping("/api/com")
+public class ComplaintController {
+
+	@Autowired
+	private IComplaintService crs;
+
+	@PostMapping("/api/bookcomplaint")
+	public ResponseEntity<?> bookComplaint(@RequestBody Client client, Complaint complaint, Product product) {
+
+		boolean b = crs.bookComplaint(client, complaint, product);
+		return new ResponseEntity<>(b, HttpStatus.CREATED);
+	}
+
+	@GetMapping(value = "//viweall-complaints", produces = "application/json")
+	public ResponseEntity<List<Complaint>> getClientAllComplaints(Client client) throws ResourceNotFoundException {
+		return new ResponseEntity<List<Complaint>>(crs.getClientAllComplaints(client), HttpStatus.OK);
+	}
+
+	@GetMapping("/getengineer/{id}")
+	public ResponseEntity<Engineer> getEngineer(@PathVariable int engineerId) throws Exception {
+		if (engineerId == 0) {
+			throw new Exception("enginerr id is invalid");
+		}
+		return new ResponseEntity<Engineer>(crs.getEngineer(engineerId), HttpStatus.OK);
+
+	}
+}
---
+++ src/main/java/com/example/ABCElectronic_smartDevice/Controller/EngineerController.java
@@ -0,0 +1,45 @@
+package com.example.ABCElectronic_smartDevice.Controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ABCElectronic_smartDevice.entity.Complaint;
+import com.example.ABCElectronic_smartDevice.entity.Engineer;
+import com.example.ABCElectronic_smartDevice.services.IEngineerService;
+
+@RestController
+@RequestMapping("/api/engineer")
+public class EngineerController {
+
+	@Autowired
+	private IEngineerService sr;
+
+	@GetMapping("/getall-opencomplaints")
+	public ResponseEntity<List<Complaint>> getAllOpenComplaints(Engineer engineer) {
+		return new ResponseEntity<List<Complaint>>(sr.getAllOpenComplaints(engineer), HttpStatus.OK);
+	}
+
+	@GetMapping("/getall-resolvedcomplaints")
+	public ResponseEntity<List<Complaint>> getResolvedComplaints(Engineer engineer) {
+		return new ResponseEntity<List<Complaint>>(sr.getResolvedComplaints(engineer), HttpStatus.OK);
+
+	}
+
+	@GetMapping("/getall-resolvedbydate")
+	public ResponseEntity<List<Complaint>> getResolvedComplaintsByDate(Engineer engineer, LocalDate date) {
+		return new ResponseEntity<List<Complaint>>(sr.getResolvedComplaintsByDate(engineer, date), HttpStatus.OK);
+	}
+
+	@GetMapping("/getallcomplaints")
+	public ResponseEntity<List<Complaint>> getComplaints(Engineer engineer, String complaintName) {
+		return new ResponseEntity<List<Complaint>>(sr.getComplaints(engineer, complaintName), HttpStatus.OK);
+	}
+
+}
